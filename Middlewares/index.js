2f598bcfe7122b191f46dae46525106a443f867a
CheemaMahesh <maheshbabucheema789@gmail.com>
2025-02-11 21:10:23 +0530

    finished signin route and created new auth middleware

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -1,32 +1,66 @@
 const { z } = require("zod");
+const jwt = require("require");
 // TODO ceate an auth middlware
 // TODO create validate all required details
 
-const isFormatValid = async (req, res, next) => {
+const isFormatValidSignUp = async (req, res, next) => {
 try{
     const requiredDetails = z.object({
         name: z.string().min(3).max(40),
         email: z.string().min(5).max(50).email(),
         password: z.string().min(6).max(35),
     });
-console.log("req.body", req.body);
     const successRequiredDetails = requiredDetails.safeParse(req.body);
-console.log("successRequiredDetails", successRequiredDetails)
     // Return error message if the format is incorrect
     if (!successRequiredDetails.success) {
         res.status(400).json({
             message: "Format is incorrect",
-            // details: successRequiredDetails.error,
+            success: false,
         })
         return;
     }
     next();
 } catch (err) {
-    console.log("something went wrong in isFormatValid middlware!", err);
+    console.log("something went wrong in isFormatValidsignup middlware!", err);
+}
 }
 
+const isFormatValidForSignIn = async (req, res, next) => {
+   try{
+    const requiredDetails = z.object({
+        email: z.string().min(3).max(40).email(),
+        password: z.string().min(5).max(50),
+        });
+    const successRequiredDetails = requiredDetails.safeParse(req.body);
+    if (!successRequiredDetails.success){
+        res.status(400).json({
+            message: "Invalid Body Format",
+            success: false,
+        });
+        return;
+    }
+    console.log("done");
+    next();
+   } catch(err) {
+    console.log("something went wrong in isFormatValidSignin middlware!", err);
+   }
+}
+
+const auth = (req, res, next) => {
+    const token = req.headers.token;
+    const exiUser = jwt.verify(token, process.env.JWT_SECRET);
+    if(!exiUser){
+        res.status(404).json({
+            message: "Unotherized Request",
+            success: false,
+        });
+        return;
+    }
+    next();
 }
 
 module.exports = {
-    isFormatValid: isFormatValid,
+    isFormatValidSignUp: isFormatValidSignUp,
+    isFormatValidForSignIn: isFormatValidForSignIn,
+    auth: auth,
 };
--- router/index.js
+++ router/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const { isFormatValid } = require("../Middlewares");
-const { signupUser } = require("../Controllers");
+const { isFormatValidSignUp, isFormatValidForSignIn } = require("../Middlewares");
+const { signupUser, signinUser } = require("../Controllers");
 
 const router = express.Router();
 
 // Todo
 // 1. signup
-    router.post('/signup', isFormatValid, signupUser);
+    router.post('/signup', isFormatValidSignUp, signupUser);
+
 // 2. signin
+    router.post('/signin', isFormatValidForSignIn, signinUser);
 // 3. Create an Expense
 // 4. Update an Expense
 // 5. Get an Expense
