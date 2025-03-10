d6f9457c96ef7e878cde10de06311c9a8388b9d4
CheemaMahesh <maheshbabucheema789@gmail.com>
2025-03-10 15:34:23 +0530

    initated expense-routes

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { validateSignupData, validateSinginData } from '../Middlewares/index';
+import { validateSignupData, validateSinginData, IsValidUser } from '../Middlewares/index';
 import { signupUser, signinUser, } from '../Controllers/user';
 
 export const authRouter = Router();
 
 authRouter.post('/signup', validateSignupData, signupUser);
 authRouter.post('/signin', validateSinginData, signinUser);
+
+authRouter.post('/create-expense', IsValidUser, () => {});
+authRouter.get('/all-expeses', IsValidUser, () => {});
+authRouter.put('/update-expense', IsValidUser, () => {});
+authRouter.delete('/delete-expense', IsValidUser, () => {});
